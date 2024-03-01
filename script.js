ed854d7fe361558601788441ae473eeb7565ee73
giftedpromise <giftedpromise1992@gmail.com>
2024-03-01 07:01:36 +0100

    JavaScript

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.querySelector(".close-btn");
 const menu = document.querySelector(".nav-links");
 const overlay = document.querySelector(".overlay");
+const mainThumbnail = document.querySelector(".main-thumbnail");
 const indicator = document.querySelector(".indicator");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+
+const previewImages = document.querySelectorAll(".preview img");
+const mainThumbnailLightBox = document.querySelector(
+  ".lightbox-container .main-thumbnail"
+);
 
 indicator.style.display = "none";
 
@@ -23,3 +31,34 @@ closeBtn.addEventListener(
   },
   { passive: false }
 ); // Set passive option to false
+
+previewImages.forEach((previewImage) => {
+  previewImage.addEventListener("click", () => {
+    const lastImg = document.querySelectorAll(".selected");
+    if (lastImg) {
+      lastImg[0].classList.remove("selected");
+    }
+    previewImage.classList.add("selected");
+    const selectedImg = document.querySelector(".selected");
+    switch (selectedImg.getAttribute("src")) {
+      case "./images/image-product-1-thumbnail.jpg":
+        mainThumbnail.src = "./images/image-product-1.jpg";
+        mainThumbnailLightBox.src = "./images/image-product-1.jpg";
+        break;
+      case "./images/image-product-2-thumbnail.jpg":
+        mainThumbnail.src = "./images/image-product-2.jpg";
+        mainThumbnailLightBox.src = "./images/image-product-2.jpg";
+        break;
+      case "./images/image-product-3-thumbnail.jpg":
+        mainThumbnail.src = "./images/image-product-3.jpg";
+        mainThumbnailLightBox.src = "./images/image-product-3.jpg";
+        break;
+      case "./images/image-product-4-thumbnail.jpg":
+        mainThumbnail.src = "./images/image-product-4.jpg";
+        mainThumbnailLightBox.src = "./images/image-product-4.jpg";
+        break;
+    }
+  });
+});
+
+mainThumbnail.addEventListener("click", openLightBox);
